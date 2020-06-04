59de25eee4bde56acca7a70b0276c0d29a064946
CataSt11 <cata.stoica11@gmail.com>
2020-06-04 20:36:29 +0000

    added the JS song list

---
+++ assets/js/songs.js
@@ -0,0 +1,109 @@
+// list of songs that will be iterated
+// the recommendations for the users will be made according to the choice took in the form
+
+const songs = [
+    {
+        artist: 'Phil Harris, Bruce Reitherman',
+        title: 'The Bare Necessities',
+        youtube_id: '08NlhjpVFsU',
+        class: 'happy'
+    },
+
+     {
+        artist: 'James Brown',
+        title: 'I Feel Good',
+        youtube_id: 'iSLwVaebsJg',
+        class: 'happy'
+    },
+
+     {
+        artist: 'Los Del Rio',
+        title: 'Macarena',
+        youtube_id: 'zWaymcVmJ-A',
+        class: 'happy'
+    },
+
+     {
+        artist: 'PSY',
+        title: 'GANGNAM STYLE',
+        youtube_id: '9bZkp7q19f0',
+        class: 'happy'
+    },
+
+     {
+        artist: 'Gusttavo Lima',
+        title: 'Balada Boa',
+        youtube_id: '5NNi4JIwsCo',
+        class: 'happy'
+    },
+
+     {
+        artist: 'Natiruts',
+        title: 'Reggae Power / Esperar o Sol',
+        youtube_id: 'NtV94qNRaTY',
+        class: 'meh'
+    },
+
+     {
+        artist: 'Damian "Jr. Gong" Marley',
+        title: 'Speak Life',
+        youtube_id: 'ebfeumBDymc',
+        class: 'meh'
+    },
+
+     {
+        artist: 'Buena Vista Social Club',
+        title: 'Chan Chan (Official Audio)',
+        youtube_id: 'o5cELP06Mik',
+        class: 'meh'
+    },
+
+     {
+        artist: 'A.CHAL',
+        title: 'Love N Hennessy',
+        youtube_id: 'Tg9sEZNkjDU',
+        class: 'meh'
+    },
+
+     {
+        artist: 'Arlissa',
+        title: 'Devil and The Deep',
+        youtube_id: 's4Bo7x4V3Is',
+        class: 'meh'
+    },
+
+     {
+        artist: 'Paula Fernandes, Marcus Viana',
+        title: 'Quando A Chuva Passar',
+        youtube_id: 'pnipW5naaFo',
+        class: 'low'
+    },
+
+     {
+        artist: 'Florence and the Machine',
+        title: 'Wish That You Were Here',
+        youtube_id: 'dsWDUvuF0Xc',
+        class: 'low'
+    },
+
+     {
+        artist: 'Carla Morrison',
+        title: 'Todo Pasa',
+        youtube_id: 'MYUVH4fflc8',
+        class: 'low'
+    },
+
+     {
+        artist: 'Sara Bareilles',
+        title: 'Gravity',
+        youtube_id: 'rEXhAMtbaec',
+        class: 'low'
+    },
+
+     {
+        artist: 'Jacob Banks ',
+        title: 'Unknown (To You)',
+        youtube_id: 'RsadnWrUY84',
+        class: 'low'
+    }
+]
