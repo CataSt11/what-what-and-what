c980c3c3ced4da76f189b659f8991d3903a97e96
CataSt11 <cata.stoica11@gmail.com>
2020-06-19 20:57:03 +0000

    added JSON file to replace object.js// small adjustments and comments for the rest of the files

--- assets/js/actions.js
+++ assets/js/actions.js
@@ -1,3 +1,9 @@
+/**
+ * JSON Data
+ * contains strings of songs, films, recipes and colors
+ */
+const dataObject = loadJSON("/assets/data.json");
+
 // ready event function
 $(document).ready(function () {
   $(document).on("click", "input[type=radio]", function () {
@@ -6,20 +12,33 @@ $(document).ready(function () {
   });
 });
 
+/**
+ * Function used to load JSON file
+ * 
+ * @param {string} file 
+ */
+function loadJSON(file) {
+  const request = new XMLHttpRequest();
+  request.open("GET", file, false);
+  request.send(null);
+  if (request.status == "200") {
+    return JSON.parse(request.responseText);
+  }
+}
 
 /**
  * function used to select and group elements according to mood
- * elements sent to the assigned html div
+ * elements are sent to the assigned html div
  *
  * @param {string} mood
  */
 function apply(mood) {
-  //get color - this is not an element, but gives color to it
+  //get color - this is not an element, but gives color to it according to the mood
   const colorCode = getColors(mood);
   const divColor = document.getElementById("divColor");
   divColor.style.backgroundColor = colorCode;
 
-  // get list of songs
+  //get list of songs
   const songList = getMusic(mood);
   updateElement("divMusic", songList, colorCode);
 
@@ -30,31 +49,30 @@ function apply(mood) {
   //get list of recipes
   const recipeList = getRecipes(mood);
   updateElement("divFood", recipeList, colorCode);
-};
-
+}
 
 /**
  * function used to update elements with recommended music, films and recipes, as well as color
- * 
- * @param {string} elementId 
- * @param {object} objectList 
- * @param {hex code} colorCode 
+ *
+ * @param {string} elementId
+ * @param {array} objectList
+ * @param {string} colorCode
  */
 function updateElement(elementId, objectList, colorCode) {
   const element = document.getElementById(elementId);
   clearElementChildren(element);
   const elementDl = createElementDL(objectList, colorCode);
   element.appendChild(elementDl);
-};
-
+}
 
 /**
- * function used to create lists of elements
- * returns text for description and links for accessing the information - taken from getMusic, getFilms, getRecipes and getColors functions below.
- * font color changed according to the mood 
- * 
- * @param {string} listObjects 
- * @param {hex code} colorCode 
+ * function used to create lists of elements from a list of objects
+ * and changing text color based on the color code
+ *
+ * @param {array} listObjects
+ * @param {string} listObjects[].url
+ * @param {string} listObjects[].text
+ * @param {string} colorCode
  */
 function createElementDL(listObjects, colorCode) {
   const elementDl = document.createElement("dl");
@@ -71,34 +89,31 @@ function createElementDL(listObjects, colorCode) {
     elementDl.appendChild(elementDd);
   }
   return elementDl;
-};
-
+}
 
 /**
- * function used to clear previously recommended music, films and recipes, in case of change of mood 
- * 
- * @param {any} element 
- */ 
+ * function used to clear element children from element
+ *
+ * @param {any} element
+ */
 function clearElementChildren(element) {
   const elementChildren = element.childNodes || [];
   for (let i = 0; i < elementChildren.length; i++) {
     element.removeChild(elementChildren[i]);
   }
-};
-
+}
 
 /**
  * music recommendation function
- * iterates through the songs list and returns the recommendations for the selected mood (html form)
- * 
- * @param {string} mood 
- */ 
+ * iterates through the songs list and returns the recommendations for the selected mood
+ *
+ * @param {string} mood
+ */
 function getMusic(mood) {
   const result = [];
-  for (let i = 0; i < songs.length; i++) {
-    const songData = songs[i];
+  for (let i = 0; i < dataObject.songs.length; i++) {
+    const songData = dataObject.songs[i];
     //if mood matches the users' selection, the function returns the song title and the artist as text and underneath a YouTube link (root + YT Id)
-    //information taken from songs object
     if (songData.mood === mood) {
       const youtubeUrl = `https://www.youtube.com/watch?v=${songData.id}`;
       const youtubeTitle = `${songData.artist} - ${songData.title}`;
@@ -109,21 +124,19 @@ function getMusic(mood) {
     }
   }
   return result;
-};
-
+}
 
 /**
  * film recommendation function
- * iterates through the films list and returns the recommendations for the selected mood (html form)
- * 
- * @param {string} mood 
+ * iterates through the films list and returns the recommendations for the selected mood
+ *
+ * @param {string} mood
  */
 function getFilms(mood) {
   const result = [];
-  for (let i = 0; i < films.length; i++) {
-    const filmData = films[i];
+  for (let i = 0; i < dataObject.films.length; i++) {
+    const filmData = dataObject.films[i];
     //if mood matches the users' selection, the function returns the film title as text and underneath a Rotten Tomatoes link (root + RT Id)
-    //information taken from films object
     if (filmData.mood === mood) {
       const filmUrl = `https://www.rottentomatoes.com${filmData.id}`;
       result.push({
@@ -133,21 +146,19 @@ function getFilms(mood) {
     }
   }
   return result;
-};
-
+}
 
 /**
  * food recommendation function
- * iterates through the recipes list and returns the recommendations for the selected mood (html form)
- * 
- * @param {string} mood 
+ * iterates through the recipes list and returns the recommendations for the selected mood
+ *
+ * @param {string} mood
  */
 function getRecipes(mood) {
   const result = [];
-  for (let i = 0; i < recipes.length; i++) {
-    const recipeData = recipes[i];
+  for (let i = 0; i < dataObject.recipes.length; i++) {
+    const recipeData = dataObject.recipes[i];
     //if mood matches the users' selection, the function returns the recipe name as text and underneath a BBCGoodFood link (root + BBCGF Id)
-    //information taken from recipes object
     if (recipeData.mood === mood) {
       const recipeUrl = `https://www.bbcgoodfood.com/recipes/${recipeData.id}`;
       result.push({
@@ -157,20 +168,19 @@ function getRecipes(mood) {
     }
   }
   return result;
-};
-
+}
 
 /**
- * function used to return the color for the selected mood (html form)
- * 
- * @param {string} mood 
+ * function used to return the color for the selected mood
+ *
+ * @param {string} mood
  */
 function getColors(mood) {
-  for (let i = 0; i < colors.length; i++) {
-    const colorData = colors[i];
+  for (let i = 0; i < dataObject.colors.length; i++) {
+    const colorData = dataObject.colors[i];
     if (colorData.mood === mood) {
       return colorData.code;
     }
   }
   return "#fff";
-};
+}
